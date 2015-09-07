bb3af8b138aae67a487974b28de9228b4c1cc723
richypunk <richy_rojas05@hotmail.com>
2015-09-07 11:08:12 -0700

    cadena
    cadena

--- Carsolio/Carsolio/Conexion.cs
+++ Carsolio/Carsolio/Conexion.cs
@@ -19,7 +19,7 @@ namespace Carsolio
      public class Conexion
      {
          
-         static string strcon = "Data Source=localhost;Initial Catalog=carsolio;Integrated Security=True";
+         static string strcon = "Data Source=.\\SQLEXPRESS;Initial Catalog=carsolio;Integrated Security=True;";
        
           SqlConnection con= new  SqlConnection(strcon);
         
